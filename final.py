82a09737ce067a01404078f44b3f6ad9be015a6b
Jetpuffed <brandon@jetpuffed.org>
2021-07-16 13:41:33 -0700

    UPDATE: Commented code

--- final.py
+++ final.py
@@ -12,24 +12,24 @@ def reconstruct_path(came_from, current):
 
 
 def a_star(graph, start, goal):
-    open_set = [(0, start)]
-    came_from = {}
-    g_score = defaultdict(lambda: math.inf)
-    g_score[start] = 0
+    open_set = [(0, start)]  # Heap queue with inital value (the start node)
+    came_from = {}  # Keeps track of path from start to goal
+    g_score = defaultdict(lambda: math.inf)  # A dictionary with infinity as the default value
+    g_score[start] = 0  # Starting value is set to zero
 
-    while (len(open_set) != 0):
-        _, current = heapq.heappop(open_set)
+    while (len(open_set) != 0):  # Repeats loop as long as open_set isn't empty
+        _, current = heapq.heappop(open_set)  # Sets current to node label
 
-        if current == goal:
+        if current == goal:  # If we've reached the goal, then it builds the path
             return reconstruct_path(came_from, current)
         
-        for neighbor, neighbor_d in graph[current]:
+        for neighbor, neighbor_d in graph[current]:  # Iterates through the node's neighbors
             tentative_g_score = g_score[current] + neighbor_d
 
-            if tentative_g_score < g_score[neighbor]:
+            if tentative_g_score < g_score[neighbor]:  # Chooses the lower score between the two values
                 came_from[neighbor] = current
                 g_score[neighbor] = tentative_g_score
 
-                if neighbor not in open_set:
+                if neighbor not in open_set:  # Pushes the node to open_set if it's not already there
                     heapq.heappush(open_set, (neighbor_d, neighbor))
-    return None
+    return None  # If no path is found, None is returned
